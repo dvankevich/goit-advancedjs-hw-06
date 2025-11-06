23b0d3ea20a2fe638528e31bec0134235f6c87ea
dvankevich <dvankevich@gmail.com>
2025-11-06 19:28:47 +0200

    bt09

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,23 +1,35 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+interface PageDetails {
+    createAt: Date;
+    updateAt: Date;
+}
+
+interface Page {
+    title: string;
+    likes: number;
+    accounts: string[];
+    status: "open" | "close";
+    details?: PageDetails;
+}
 
 const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: new Date("2021-01-01"),
+        updateAt: new Date("2021-05-01"),
+    },
+};
 
 const page2 = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
 
 export {};
